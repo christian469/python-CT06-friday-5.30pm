751e23d4640f35fea76f9e731ccf07699deb16b5
christian69 <christianisapro28@gmail.com>
2026-01-16 10:47:27 +0000

    end of lesson 2

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,50 @@
 print("Hello from lesson 2")
 
+print("red")
+# print("orange")
+# print("yellow")
+# print("green")
+# print("blue")
+# print("indigo")
+# print("violet")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
+# repeat 10 times:
+# say "HAIIIIIII!!!!"
+# move 10 step
+# 
+# assign 0 to "counter"
+# while "counter" is not 50
+# move 10 step
+# turn 15 degrees
+# change counter by 10
+# 
+# ask "what is ur age" and assign to ans
+# if ans lower than "18" then
+# say Access Denied!>:)
+# otherwise, 
+# say WELECOME:)
+
+# task 2 >:(
+# create 3 list (bins), one for glass, plastic and paper
+# accept one recycled item from the user
+# scan xray the type of material
+# if material is glass then glass list
+# if material is plastic then put to plastic list
+# if material is paper then put to paper list 
+# otherwise, reject item
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+# secretPhrase ← ("open sesame")
+
+# display "Enter the secret phrase:"
+# userInput ← READ input
+
+# if userInput = secretPhrase THEN
+    # display "Access granted"
+# else
+    # display "Access denied"
+
+
+
