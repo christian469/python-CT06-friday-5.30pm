1839ef4856bb00bd05ff19fe232913ceac020259
christian69 <christianisapro28@gmail.com>
2026-02-06 10:49:08 +0000

    end of lesson 5

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,22 @@
-print("Hello from lesson 5")
+# print("Hello from lesson 5")
+# name = input("what is ur name? tell me now")
+# age = input("what is ur age? TELL ME NOW") 
+# message = input("tell me where ur parents live OR ELSE")
+# print("happy birthday" + name + "ur age is" + age + "u live at" + message)
+
+
+# for count in range (1, 6):
+#     print(count)
+
+# for count in range ( 51, 101):
+#     print(count)
+
+# for count in range (18, 30):
+#     print(count)
+
+
+urname = input("give me a name: ")
+for letter in urname:
+    print("give me a", letter.upper(), "!")
+
+print(urname, "is the best!")
