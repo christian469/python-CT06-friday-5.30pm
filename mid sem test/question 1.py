64f55c1ed6700f172b86aeb3892c22a1f1f08a8d
christian69 <christianisapro28@gmail.com>
2026-03-06 10:35:24 +0000

    mid sem test

---
+++ mid sem test/question 1.py
@@ -0,0 +1,38 @@
+# Q1. Bill Splitter
+
+
+# Step 1: Ask for Total Bill
+bill = input("what is the total bill")
+
+
+
+
+
+# ============================================================
+# Step 2: Ask for Number of People
+# ============================================================
+people = input("how many people are there")
+
+
+# ============================================================
+# Step 3: Calculate Equal Split
+print(bill/people)
+
+
+
+
+# ============================================================
+# - Divide total bill by number of people
+# - Store result in a variable
+# ============================================================
+
+
+
+# ============================================================
+# Step 4: Print Final Result
+# ============================================================
+# - Print the result in this format:
+#   Each person pays: $<amount>
+#   Rounded to 2 decimal places
+# ============================================================
+    
---
+++ mid sem test/question 2.py
@@ -0,0 +1,55 @@
+# The program must:
+# - Ask for the starting amount of money.
+# - Ask for the number of days.
+# - For each day, add the correct savings amount.
+# - Print the total money after each day.
+# - Finally, print the final total amount.
+
+# Program Requirements:
+# - Use a for loop.
+# - Use range() correctly.
+# - Update the total amount inside the loop.
+# - Print exactly in this format:
+#     Day <X>: $<Y>
+# - After the loop, print:
+#     Total amount saved = $<Z>
+
+# Note:
+# - Follow the input order exactly as shown in the Test Cases.
+# - You must get the correct output for ALL 3 test cases.
+
+
+# Step 1: Ask for Starting Amount
+start = input("how much do u currently have?")
+int(start)
+print("i currently have", start)
+
+
+
+
+# Step 2: Ask for Number of Days
+day = input("how many days do u want?")
+int(day)
+print("I want to have", day, "days")
+
+
+
+
+# Step 3: Use a for loop to simulate savings
+
+# - Use range() correctly
+# - Add the correct daily savings amount
+# - Update and print the total each day
+#   Day <X>: $<Y>
+for range in (start):
+    
+    print(range)
+
+
+
+
+
+# Step 4: Print Final Total
+
+# - Print the final amount in this format:
+#   Total amount saved = $<Z>
