c034a657bd69d88ede09ab9d64b3a80eeeefec3e
christian69 <christianisapro28@gmail.com>
2026-04-12 09:35:14 +0000

    end of lesson 12

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,1 +1,48 @@
-print("Hello from lesson 12")
+# print("Hello from lesson 12")
+
+# user = int(input("enter a number"))
+# if (user % 3 == 0 and user % 5==0):
+#     print("the number is divisable by 3 and 5")
+# else:
+#     print("number is not divisable by 3 and 5")
+
+# visitors = int(input("visitors already present:"))
+# maxvisitors = int(input("max visitors allowed:"))
+# while visitors < maxvisitors:
+#     visitors += 1
+#     print(visitors)
+
+# resturantvisitor = 0
+# while True:
+#     resturantvisitor +=1
+#     print(resturantvisitor)
+#     if resturantvisitor ==30:
+#         break
+
+# order = ""
+# orderr = int(input("what is ur order"))
+# while True:
+#     print(orderr)
+#     break
+number = 10
+while number -0:
+    print(number)
+    number -= 1
+    if number == 5:
+        break
+else:
+    print("HAPPYNEWYEAR:")
+
+import random 
+score = 0
+count = 10
+while random:
+    number1 = random.randint(1,30)
+    number2 = random.randint(1,30)
+    user = input("what is "  + str(number1) + "+" + str(number2))
+    if int(user) == (number1 + number2) : 
+        print("thats correct such a good boi")
+        score+=1
+    else:
+        print("wrong")
+    count-= 1
