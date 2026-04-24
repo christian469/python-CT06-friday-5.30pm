131021b7054597c8f8dcfc4f3c5e1a79af8df03f
christian69 <christianisapro28@gmail.com>
2026-04-24 10:55:33 +0000

    end of lesson 13

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,50 @@
-print("Hello from lesson 13")
+# print("Hello from lesson 13")
+# counter = 25
+# while counter < 50:
+#     print(counter)
+#     counter += 1
+
+
+# counter = 100
+# while counter > 79:
+#     print(counter)
+#     counter -= 1
+
+
+# counter = 5
+# while counter < 101:
+#     print (counter)
+#     counter += 5
+
+
+# items = ["apples", "bread", "carrots", "dates", "eggs", "flour", "grapes", "honey"]
+# # print (items)
+# items[7] = "herbs"
+# items.append("ice")
+# items.insert(1, "banana")
+# del(items[2])
+# for i in items:
+#     print ("i need this item NOW" , i)
+# for i in items:
+#     if i == "apples":
+#         print ("i need 5 of these" , i)
+#     if i == "carrots":
+#         print("i need 3 of these" , i)
+#     if i == "grapes":
+#         print("get the FramFresh brand")
+
+
+# namelist = ["aric", "jace", "christian", "me", "myself", "and", "I"]
+# for n in namelist:
+#     print (n)
+
+
+question = "what do u call a bear without its ear"
+print(question)
+ans=input("what is ur anwser")
+hidden = "b"
+HIDDEN2 = "B"
+if ans == hidden or ans == HIDDEN2:
+    print ("correct")
+else:
+    print("wrong")
