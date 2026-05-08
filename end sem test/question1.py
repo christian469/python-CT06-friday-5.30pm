7066269ba1fe3cf5b53f40d9e470318a61e4d3fe
christian69 <christianisapro28@gmail.com>
2026-05-08 10:45:48 +0000

    end sem test

---
+++ end sem test/question1.py
@@ -0,0 +1,18 @@
+# a)The program must print numbers from 10 to 200 using a while loop.
+# b)The numbers must increase in multiples of 10.
+while range(10,200):
+    print (range)
+    break
+##################################################
+# - Store the password "superpass123"
+# - Ask the user to enter a password
+# - If correct, print:
+#     Access Granted
+# - If wrong, print:
+#     Access Denied
+password = "superpass123"
+input("what is the password")
+if password:
+    print ("access granted")
+else:
+    print("access denied")
---
+++ end sem test/question2.py
@@ -0,0 +1,15 @@
+# Program Requirements:
+# - Use the list:
+#   planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus"]
+# - Print the 3rd item using index
+# - Append "neptune" to the list
+# - Rename "mars" to "muskworld"
+# - Remove "uranus" from the list
+# - Using a for loop, print all the planets one by one
+planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus"]
+
+print(planets[2])
+# planets.append("neptune")
+# print (planets)
+del planets[6]
+print (planets)
